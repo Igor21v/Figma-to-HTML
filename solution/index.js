662c8f023305d1086a1aa321faf508556fa4524d
Igor Bondarenko <igor21v@mail.ru>
2023-10-24 22:07:02 +0800

    build: prepair

--- solution/index.js
+++ solution/index.js
@@ -1,9 +1,46 @@
-/**
- * Если нет идей, загляните в папку ./example
- * В файле solution.js будет пример превращения компонента Text в HTML.
- */
-module.exports = function(json) {
-    /**
-     * Здесь будет ваше решение
-     */
-}
+const TEXT_STYLES_MAPPER = {
+  fontSize: (value) => `font-size: ${value}px;`,
+  fontWeight: (value) => `font-weight: ${value};`,
+  textAlignHorizontal: (value) => `text-align: ${value.toLowerCase()};`,
+};
+
+const buildBlock = ({ type, content, className, style }) => {
+  return `<${type} class="${className}" style="${style}">${content}</${type}>`;
+};
+
+const getTextStyles = (node) => {
+  const styleArr = [];
+  if (node.style) {
+    for (let [key, value] of Object.entries(node.style)) {
+      if (TEXT_STYLES_MAPPER[key]) {
+        styleArr.push(TEXT_STYLES_MAPPER[key](value));
+      }
+    }
+  }
+  return styleArr.join(" ");
+};
+
+const PRIMITIVES = {
+  TEXT: (node) => {
+    return buildBlock({
+      type: "span",
+      content: node.characters,
+      className: node.type,
+      style: getTextStyles(node),
+    });
+  },
+};
+
+const parse = (entry) => {
+  return traverse(entry.children[0]);
+};
+
+const traverse = (node) => {
+  // тут надо придумать, как обходить дерево:)
+  return PRIMITIVES[node.type](node);
+};
+
+module.exports = function (json) {
+  const entry = json.document.children[0];
+  return parse(entry);
+};
