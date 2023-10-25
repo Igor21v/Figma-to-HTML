825b610be9d23516fab35b130edee1271ef06487
Igor Bondarenko <igor21v@mail.ru>
2023-10-25 22:09:47 +0800

    build: add getDivStyles

--- solution/index.js
+++ solution/index.js
@@ -1,67 +1,93 @@
-let stack = [];
+const stack = [];
 
 const TEXT_STYLES_MAPPER = {
-  fontSize: (value) => `font-size: ${value}px;`,
-  fontWeight: (value) => `font-weight: ${value};`,
-  textAlignHorizontal: (value) => `text-align: ${value.toLowerCase()};`,
+    fontSize: (value) => `font-size: ${value}px;`,
+    fontWeight: (value) => `font-weight: ${value};`,
+    textAlignHorizontal: (value) => `text-align: ${value.toLowerCase()};`,
 };
 
 const getTextStyles = (node) => {
-  const styleArr = [];
-  if (node.style) {
-    for (let [key, value] of Object.entries(node.style)) {
-      if (TEXT_STYLES_MAPPER[key]) {
-        styleArr.push(TEXT_STYLES_MAPPER[key](value));
-      }
+    const styleArr = [];
+    if (node.style) {
+        for (let [key, value] of Object.entries(node.style)) {
+            if (TEXT_STYLES_MAPPER[key]) {
+                styleArr.push(TEXT_STYLES_MAPPER[key](value));
+            }
+        }
     }
-  }
-  return styleArr.join(" ");
+    return styleArr.join(' ');
+};
+
+const getDivStyles = (node) => {
+    const styleArr = ['display: flex;'];
+    for (let [key, value] of Object.entries(node)) {
+        switch (key) {
+            case 'itemSpacing':
+                styleArr.push(`gap: ${value}px;`);
+                break;
+            case 'layoutMode':
+                value === 'VERTICAL' &&
+                    styleArr.push('flex-direction: column;');
+                break;
+            case 'primaryAxisAlignItems':
+                value === 'SPACE_BETWEEN' &&
+                    styleArr.push('justify-content: space-between;');
+                value === 'CENTER' && styleArr.push('justify-content: center;');
+                break;
+            case 'counterAxisAlignItems':
+                value === 'CENTER' && styleArr.push('align-items: center;');
+                break;
+            default:
+                break;
+        }
+    }
+    return styleArr.join(' ');
 };
 
 const buildDiv = (node) => {
-  const openTag = `<div class="${node.type}" style="${getTextStyles(
-    node
-  )}" id=${node.name}>`;
-  if (node.children) {
-    if (node.closingTag) {
-      node.children.at(-1).closingTag = node.closingTag + "</div>";
-      node.closingTag = null;
-    } else {
-      node.children.at(-1).closingTag = "</div>";
+    const openTag = `<div class="${node.type}" 
+                    style="${getDivStyles(node)}" 
+                    id=${node.name}>`;
+    if (node.children) {
+        if (node.closingTag) {
+            node.children.at(-1).closingTag = node.closingTag + '</div>';
+            node.closingTag = null;
+        } else {
+            node.children.at(-1).closingTag = '</div>';
+        }
+        stack.push(...node.children.reverse());
+        return openTag;
     }
-    stack.push(...node.children.reverse());
-    return openTag;
-  }
 
-  return openTag + "</div>";
+    return openTag + '</div>';
 };
 
 const PRIMITIVES = {
-  TEXT: (node) => {
-    return `
+    TEXT: (node) => {
+        return `
       <span class="${node.type}" style="${getTextStyles(node)}">
       ${node.characters}
       </span>`;
-  },
-  FRAME: (node) => buildDiv(node),
-  INSTANCE: (node) => buildDiv(node),
-  RECTANGLE: (node) => buildDiv(node),
+    },
+    FRAME: (node) => buildDiv(node),
+    INSTANCE: (node) => buildDiv(node),
+    RECTANGLE: (node) => buildDiv(node),
 };
 
 const parse = (entry) => {
-  let html = "";
-  stack.push(entry.children[0]);
-  while (stack.length) {
-    const node = stack.pop();
-    html += PRIMITIVES[node.type](node);
-    if (node.closingTag) {
-      html += node.closingTag;
+    let html = '';
+    stack.push(entry.children[0]);
+    while (stack.length) {
+        const node = stack.pop();
+        html += PRIMITIVES[node.type](node);
+        if (node.closingTag) {
+            html += node.closingTag;
+        }
     }
-  }
-  return html;
+    return html;
 };
 
 module.exports = function (json) {
-  const entry = json.document.children[0];
-  return parse(entry);
+    const entry = json.document.children[0];
+    return parse(entry);
 };
