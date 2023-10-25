c1005eb2063486a42d3d7b83aed8054a7a4ea24d
Igor Bondarenko <igor21v@mail.ru>
2023-10-25 15:05:32 +0800

    build: add cycle while

--- solution/index.js
+++ solution/index.js
@@ -1,11 +1,9 @@
+let stack = [];
+
 const TEXT_STYLES_MAPPER = {
   fontSize: (value) => `font-size: ${value}px;`,
   fontWeight: (value) => `font-weight: ${value};`,
   textAlignHorizontal: (value) => `text-align: ${value.toLowerCase()};`,
-};
-
-const buildBlock = ({ type, content, className, style }) => {
-  return `<${type} class="${className}" style="${style}">${content}</${type}>`;
 };
 
 const getTextStyles = (node) => {
@@ -22,22 +20,49 @@ const getTextStyles = (node) => {
 
 const PRIMITIVES = {
   TEXT: (node) => {
+    return `
+      <span class="${node.type}" style="${getTextStyles(node)}">
+      ${node.characters}
+      </span>`;
+  },
+  FRAME: (node) => {
+    const openTag = `<div class="${node.type}" style="${getTextStyles(node)}">`;
+    if (node.children) {
+      stack.push(...node.children);
+      stack.at(-1).closingTag = "</div>";
+      return openTag;
+    }
+    return openTag + "</div>";
+  },
+  /*   INSTANCE: (node) => {
     return buildBlock({
-      type: "span",
+      type: "div",
       content: node.characters,
       className: node.type,
       style: getTextStyles(node),
     });
   },
+  RECTANGLE: (node) => {
+    return buildBlock({
+      type: "div",
+      content: node.characters,
+      className: node.type,
+      style: getTextStyles(node),
+    });
+  }, */
 };
 
 const parse = (entry) => {
-  return traverse(entry.children[0]);
-};
-
-const traverse = (node) => {
-  // тут надо придумать, как обходить дерево:)
-  return PRIMITIVES[node.type](node);
+  let html = "";
+  stack.push(entry.children[0]);
+  while (stack.length) {
+    const node = stack.pop();
+    html += PRIMITIVES[node.type](node);
+    if (node.closingTag) {
+      html += node.closingTag;
+    }
+  }
+  return html;
 };
 
 module.exports = function (json) {
