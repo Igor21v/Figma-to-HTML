68fbc4cbd89b414c9edfc616cd798c7f33fd49b8
Igor Bondarenko <igor21v@mail.ru>
2023-10-26 16:03:07 +0800

    build: add properties

--- solution/index.js
+++ solution/index.js
@@ -1,18 +1,44 @@
 const stack = [];
 
 const TEXT_STYLES_MAPPER = {
+    fontFamily: (value) => `font-family: '${value}';`,
     fontSize: (value) => `font-size: ${value}px;`,
     fontWeight: (value) => `font-weight: ${value};`,
     textAlignHorizontal: (value) => `text-align: ${value.toLowerCase()};`,
+    fills: (value) => `color: rgba(
+      ${255 * value.color.r}, 
+      ${255 * value.color.g}, 
+      ${255 * value.color.b}, 
+      ${value.color.a});`,
 };
 
 const getTextStyles = (node) => {
     const styleArr = [];
-    if (node.style) {
-        for (let [key, value] of Object.entries(node.style)) {
-            if (TEXT_STYLES_MAPPER[key]) {
-                styleArr.push(TEXT_STYLES_MAPPER[key](value));
-            }
+    for (let [key, value] of Object.entries(node)) {
+        switch (key) {
+            case 'style':
+                for (let [key, value] of Object.entries(node.style)) {
+                    if (TEXT_STYLES_MAPPER[key]) {
+                        styleArr.push(TEXT_STYLES_MAPPER[key](value));
+                    }
+                }
+                break;
+            case 'fills':
+                styleArr.push(
+                    `color: rgba(
+                  ${255 * value[0].color?.r}, 
+                  ${255 * value[0].color?.g}, 
+                  ${255 * value[0].color?.b}, 
+                  ${value[0].color?.a});`,
+                );
+                break;
+            case 'absoluteBoundingBox':
+                if (node.style?.textAutoResize === 'HEIGHT') {
+                    styleArr.push(`width: ${value.width}px;`);
+                }
+                break;
+            default:
+                break;
         }
     }
     return styleArr.join(' ');
@@ -36,6 +62,45 @@ const getDivStyles = (node) => {
                 break;
             case 'counterAxisAlignItems':
                 value === 'CENTER' && styleArr.push('align-items: center;');
+                break;
+            case 'paddingLeft':
+                styleArr.push(`padding-left: ${value}px;`);
+                break;
+            case 'paddingRight':
+                styleArr.push(`padding-right: ${value}px;`);
+                break;
+
+            case 'paddingTop':
+                styleArr.push(`padding-top: ${value}px;`);
+                break;
+
+            case 'paddingBottom':
+                styleArr.push(`padding-bottom: ${value}px;`);
+                break;
+            case 'backgroundColor':
+                styleArr.push(
+                    `background-color: rgba(
+                      ${255 * value.r}, 
+                      ${255 * value.g}, 
+                      ${255 * value.b}, 
+                      ${value.a});`,
+                );
+                break;
+            case 'strokes':
+                if (value.length) {
+                    const stroke = value[0];
+                    console.log(stroke);
+                    styleArr.push(
+                        `border: 
+                        ${node.strokeWeight}px
+                        ${stroke.type.toLowerCase()}
+                        rgba(
+                          ${255 * stroke.color.r}, 
+                          ${255 * stroke.color.g}, 
+                          ${255 * stroke.color.b}, 
+                          ${stroke.color.a});`,
+                    );
+                }
                 break;
             default:
                 break;
@@ -75,7 +140,9 @@ const PRIMITIVES = {
 };
 
 const parse = (entry) => {
-    let html = '';
+    let html = `<link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    `;
     stack.push(entry.children[0]);
     while (stack.length) {
         const node = stack.pop();
